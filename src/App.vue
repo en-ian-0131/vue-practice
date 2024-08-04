2877a8703e32567196f126ee7192c9d84b5a8591
en-ian-0131 <DaiMingZer@gmail.com>
2024-08-04 15:30:14 +0800

    counter

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>{{ msg }}</div>
+  <div class="col-auto row gx-2 align-items-center">
+    <button class="col-auto btn btn-primary" @click="minusCount">-</button>
+    <div class="col-auto">{{ count }}</div>
+    <button class="col-auto btn btn-primary" @click="addCount">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello-world",
+  data() {
+    return {
+      msg: "Hello world123!",
+      count: 0,
+    };
+  },
+  methods: {
+    addCount() {
+      this.count = this.count + 1;
+    },
+    minusCount() {
+      if (this.count > 0) {
+        this.count = this.count - 1;
+      }
+    },
+  },
+};
+</script>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// const vm = createApp(App);
+const vm = createApp(App);
+
+vm.mount("#app");
